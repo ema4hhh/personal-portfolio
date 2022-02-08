033c5a20fbe743df78d5a8fe58be6f888f430323
ema4hhh <ema4hhh@gmail.com>
2022-02-07 21:38:28 -0500

    Presentation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './styles/App.css';
 import React from 'react';
 import { NavBar } from './components/NavBar';
 import { PresentationText } from './components/PresentationText';
-;
-
 
 function App() {
   return (
      <div className='App'>
        <NavBar />
+       <PresentationText />
      </div>
   );
 }
--- src/components/PresentationText.js
+++ src/components/PresentationText.js
@@ -1,8 +1,21 @@
+import '../styles/PresentationText.css';
+import { Grid, Typography } from "@mui/material"
+
 export const PresentationText = () => {
   return(
-    <>
-      
-    </>
-  )
+    <div className='root'>
+      <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Typography variant="body1">
+        Since beginning my journey as a freelance designer nearly 10 years ago
+        </Typography>
+        <Typography variant="body1" className='presentation'>
+        I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. 
+        </Typography>
+        <Typography variant="body1" className='presentation'>
+        I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+        </Typography>
+      </Grid>
+    </div>
+  );
 };
 
