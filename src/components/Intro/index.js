41d2823ce69aad2c4317315c2f32616ad1a00699
ema4hhh <ema4hhh@gmail.com>
2022-03-27 21:16:18 -0500

    Intro component better writen

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -3,10 +3,13 @@ import React, { useEffect, useRef } from 'react';
 //Typing animation library
 import Typed from "typed.js";
 
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { useTheme } from "@mui/material/styles";
 
 import GoToContent from './GoToContent';
+
+import MyImage from '../../assets/asd.png';
+import { height } from '@mui/system';
 
 const Intro = ({ trigger, handleGoToContent }) => {
   const theme = useTheme();
@@ -15,7 +18,7 @@ const Intro = ({ trigger, handleGoToContent }) => {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["<b>I'm a passionate and young developer</b>, <br /> working mainly in web technologies, delivering quality work."],
+      strings: ["<b>I'm a passionate and young developer</b>, <br /> working mainly in web technologies,<br /> delivering quality work."],
       typeSpeed: 50,
       cursorChar: "_",
     });
@@ -26,41 +29,39 @@ const Intro = ({ trigger, handleGoToContent }) => {
   
   return (
     <Box id="home" style={{
+      display: "flex",
       position: "relative",
       height: "94vh",
       width: "100%",
       backgroundColor: theme.palette.primary.main,
       color: theme.palette.fontColor.primary.main,
     }}>
-      <Box style={{
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        display: "table",
-        textAlign: "center",
-      }}>
-        <Box style={{
-          display: "table-cell",
-          verticalAlign: "middle",
-        }}>
+      <Grid 
+        container
+        direction="row"
+        justifyContent={"space-around"}
+        alignContent="center"
+      >
+        <Grid item xs={6}>
           <Box sx={{
-            display: "inline-block",
-            textAlign: "start"
+            marginLeft: "45vh"
           }}>
-            <Typography style={{
-              fontWeight: 500,
-              fontSize: "3.5rem",
-              marginBottom: "20px"
-            }}>
-              Hi, I'm Emanuel
-            </Typography>
-            <Typography variant='h5' sx={{
-              color: "rgb(141,154,166)",
-              fontSize: "1.5rem",
-              marginBottom: "20px"
-            }}>
-              FULLSTACK DEVELOPER
-            </Typography>
+            <Box>
+              <Typography style={{
+                fontWeight: 500,
+                fontSize: "3.5rem",
+                marginBottom: "20px"
+              }}>
+                Hi, I'm Emanuel
+              </Typography>
+              <Typography variant='h5' sx={{
+                color: "rgb(141,154,166)",
+                fontSize: "1.5rem",
+                marginBottom: "20px"
+              }}>
+                FULLSTACK DEVELOPER
+              </Typography>
+            </Box>
             <Box sx={{
               width: "600px"
             }}>
@@ -71,10 +72,15 @@ const Intro = ({ trigger, handleGoToContent }) => {
                 lineHeight: "1.25"
               }}></Typography>
             </Box>
-            
           </Box>
-        </Box>
-      </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img src={MyImage} width={""} height={"350vh"} alt="hmm, something didn't load here" sx={{
+            marginLeft: "10vh"
+          }}/>
+        </Grid>
+      </Grid>
+      
       <GoToContent trigger={trigger} handleGoToContent={handleGoToContent}/>
     </Box>
   )
