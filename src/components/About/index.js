c5361c906df641605658b431835c5d0840213388
ema4hhh <ema4hhh@gmail.com>
2022-03-27 21:15:40 -0500

    About component improved

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { Box, Toolbar, Typography, useTheme } from '@mui/material';
+import { Box, Grid, Toolbar, Typography, useTheme } from '@mui/material';
+
+import illustration from "../../assets/Hand-holding-pen-rafiki.svg"
 
 const Intro = () => {
 
@@ -9,11 +11,32 @@ const Intro = () => {
   return (
     <Box id="about" sx={{
       backgroundColor: theme.palette.primary.main,
-      color: theme.palette.fontColor.primary.main
+      color: theme.palette.fontColor.primary.main,
       }}>
       <Toolbar id="go-to-content-anchor" />
-      <Typography variant='h1'>About Me</Typography>
-      <Typography variant='h4'>Intro ingeniosa Emanuel Castaño</Typography>    
+      <Box sx={{
+        width: "90vh",
+        margin: "auto",
+      }}>
+        <Typography variant='h1' sx={{
+          marginBottom: "3vh"
+        }}>About Me</Typography>
+        <Grid container>
+          <Grid item xs={6}>
+            <img src={illustration} alt="something is not showing here..." width={"400vh"} height={"400vh"} />
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant='h4' sx={{
+              marginTop: "10vh",
+              textAlign: "left",
+              lineHeight: 1.5
+            }}>
+              Self-taught developer and data science student, passionate about software development, technology and maths. <br />
+              Currently on high school, and hatting it
+            </Typography> 
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   )
 }
