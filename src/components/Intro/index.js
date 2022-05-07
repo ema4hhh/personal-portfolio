e124a8b3a4f4c53a2212e3825a45de416a61da83
ema4hhh <ema4hhh@gmail.com>
2022-05-07 12:58:34 -0500

    put skills in Intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useRef } from 'react';
 import Typed from "typed.js";
 
 // MUI
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, Stack } from '@mui/material';
 import { useTheme } from "@mui/material/styles";
 
 // Components
@@ -35,6 +35,8 @@ const Intro = ({ trigger, handleGoToContent }) => {
       position: "relative",
       height: "94vh",
       width: "100%",
+      alignItems: "center",
+      justifyContent: "center"
     }}>
       <Grid 
         container
@@ -42,18 +44,18 @@ const Intro = ({ trigger, handleGoToContent }) => {
         justifyContent={"space-around"}
         alignContent="center"
       >
-        <Grid item xs={6}>
+        <Grid item xs={5}>
           <Box sx={{
             marginLeft: "45vh",
           }}>
             <Box>
               <Typography sx={{
                 fontWeight: 500,
-                fontSize: "3.5rem",
+                fontSize: "3.0rem",
                 marginBottom: "20px",
                 color: "text.primary",
               }}>
-                Hey! You found my portfolio
+                Hey!<br /> You found my portfolio
               </Typography>
             </Box>
             <Box sx={{
@@ -68,8 +70,79 @@ const Intro = ({ trigger, handleGoToContent }) => {
             </Box>
           </Box>
         </Grid>
-        <Grid item xs={6}>
-          
+        <Grid item xs={7}>
+          <Stack direction="row" spacing={3}>
+            <Typography
+              sx={{
+                fontSize: "2.5rem",
+                color: "#F72585"
+              }}
+            >
+              ReactJS
+            </Typography>
+            <Typography 
+              sx={{
+                fontSize: "1.6rem",
+                color: "#B5179E"
+            }}>
+              NodeJS
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={3}>
+            <Typography
+              sx={{
+                fontSize: "1.2rem",
+                color: "#480CA8"
+              }}>
+              Express
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "1.5rem",
+                color: "#3A0CA3"
+              }}>
+              PostgreSQL
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "1.8rem",
+                color: "#560BAD"
+              }}>
+              Git
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={3}>
+            <Typography
+              sx={{
+                fontSize: "1.5rem",
+                color: "#4895EF"
+              }}>
+              PowerBI
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "1.8rem",
+                color: "#4361EE"
+              }}>
+              SQL Server
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={3}>
+            <Typography
+              sx={{
+                fontSize: "1.4rem",
+                color: "#4361EE"
+              }}>
+              GitHub
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "2rem",
+                color: "#480CA8"
+              }}>
+              MaterialUI
+            </Typography>
+          </Stack>
         </Grid>
       </Grid>
       
