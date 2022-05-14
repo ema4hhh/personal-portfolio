9653d0bb137435bfbd6cbf3787febb372dfec305
ema4hhh <ema4hhh@gmail.com>
2022-05-14 08:55:55 -0500

    about updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,12 +14,12 @@ const Intro = () => {
       <Toolbar id="go-to-content" />
 
       <Box sx={{
-        width: "90vh",
+        width: "35rem",
         margin: "auto",
       }}>
         <Typography variant='h1' sx={{
           marginBottom: "3vh"
-        }}>About Me</Typography>
+        }}>Who is this guy?</Typography>
         <Grid container>
           <Grid item xs={12}>
             <Typography variant='h4' sx={{
@@ -27,8 +27,8 @@ const Intro = () => {
               textAlign: "center",
               lineHeight: 1.5
             }}>
-              Self-taught developer and data science student, passionate about software development, technology and maths. <br />
-              Currently on high school, and hatting it
+              Emanuel is a high school and CoderHouse student who is interested in full-stack development, data analytics, and data science. Emanuel's enthusiasm for technology and mathematics is contagious. He sees himself as a "eternal student" who is ready to both learn and help others.<br /> <br/>
+              Emanuel's thirst for knowledge, analytical ability, and resilience have lead him to a happier life, doing what he enjoys, coding.
             </Typography> 
           </Grid>
         </Grid>
