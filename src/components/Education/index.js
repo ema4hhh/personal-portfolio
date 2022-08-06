3d428fcf4052e37d84a6a6580543e5e60c5cbf38
ema4hhh <ema4hhh@gmail.com>
2022-08-06 00:06:00 -0500

    removed bubbles color and add new TimelineEvent

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -8,23 +8,25 @@ import { DiPython } from "react-icons/di";
 import { MdBrush } from "react-icons/md";
 import { BsBraces } from "react-icons/bs";
 import { SiMicrosoftsqlserver } from "react-icons/si";
+import { HiTranslate } from "react-icons/hi"
 
 // Timeline library
 import { Timeline, TimelineEvent } from 'react-event-timeline'
 
 const Education = () => {
+  
+  // eslint-disable-next-line no-unused-vars
   const theme = useTheme()
 
   return (
     <Box sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignContent: "center",
-      marginTop: "10vh"
+      width: "75%",
+      margin: "auto",
+      marginTop: "10vh",
     }}>
+      {/* Need this just for the font color, it doesn't apply when declared above */}
       <Box sx={{
         color: "white",
-        display: "inline-block"
       }}>
         <Timeline lineColor="rgba(255,255,255,.3)">
 
@@ -33,47 +35,67 @@ const Education = () => {
             titleStyle={{color: "#ffffff", fontWeight: 600, fontSize: "17px"}}
             subtitle="2020-02-01"
             subtitleStyle={{color: "text.secondary", fontWeight: 600}}
-            contentStyle={{color: "black", fontWeight: 600}}
+            contentStyle={{backgroundColor: "background.default", color: "#bbbbbb", fontWeight: 600}}
             icon={<DiPython size="5rem" color="rgb(2, 136, 209)" />}
-            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "rgba(255,255,255,.3)", boxShadow: "#FFF 0px 0px 2px, #FFF 0px 0px 3px, #FFF 0px 0px 5px, #FF2D95 0px 0px 7px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 25px, #FF2D95 0px 0px 20px"}}
+            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "#bbbbbb"}}
           >
             Learning basic Python and OOP with personal teacher
           </TimelineEvent>
+
+          <br />
 
           <TimelineEvent
             title="Javascript, Html and Css3"
             titleStyle={{color: "#ffffff", fontWeight: 600, fontSize: "17px"}}
             subtitle="2020-11-01"
             subtitleStyle={{color: "#faebd7", fontWeight: 600}}
-            contentStyle={{color: "black", fontWeight: 600}}
+            contentStyle={{backgroundColor: "background.default", color: "#bbbbbb", fontWeight: 600}}
             icon={<MdBrush size="1.6rem" color="rgb(2, 136, 209)" />}
-            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "rgba(255,255,255,.3)", boxShadow: "#FFF 0px 0px 2px, #FFF 0px 0px 3px, #FFF 0px 0px 5px, #FF2D95 0px 0px 7px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 25px, #FF2D95 0px 0px 20px"}}
+            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "#bbbbbb"}}
           >
             With the same teacher I started to make simple html and css pages and learning basic Javascript
           </TimelineEvent>
+          
+          <br />
 
           <TimelineEvent
             title="React, Redux, Express and PosgreSQL"
             titleStyle={{color: "#ffffff", fontWeight: 600, fontSize: "17px"}}
             subtitle="2021-03-01"
             subtitleStyle={{color: "#faebd7", fontWeight: 600}}
-            contentStyle={{color: "black", fontWeight: 600}}
+            contentStyle={{backgroundColor: "background.default", color: "#bbbbbb", fontWeight: 600}}
             icon={<BsBraces size="1.5rem" color="rgb(2, 136, 209)" />}
-            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "rgba(255,255,255,.3)", boxShadow: "#FFF 0px 0px 2px, #FFF 0px 0px 3px, #FFF 0px 0px 5px, #FF2D95 0px 0px 7px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 25px, #FF2D95 0px 0px 20px"}}
+            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "#bbbbbb"}}
           >
             I learned basic React, Redux, Express and PosgreSQL
           </TimelineEvent>
-
+          
+          <br />
+          
           <TimelineEvent
-            title="PowerBI, SQL Server, Algorithms and Machine Learning"
+            title="PowerBI, SQL Server, Pandas, Seaborn, Algorithms, Machine Learning"
             titleStyle={{color: "#ffffff", fontWeight: 600, fontSize: "17px"}}
             subtitle="2022-04-05"
             subtitleStyle={{color: "#faebd7", fontWeight: 600}}
-            contentStyle={{color: "black", fontWeight: 600}}
+            contentStyle={{backgroundColor: "background.default", color: "#bbbbbb", fontWeight: 600}}
             icon={<SiMicrosoftsqlserver size="1.5rem" color="rgb(2, 136, 209)" />}
-            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "rgba(255,255,255,.3)", boxShadow: "#FFF 0px 0px 2px, #FFF 0px 0px 3px, #FFF 0px 0px 5px, #FF2D95 0px 0px 7px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 10px, #FF2D95 0px 0px 25px, #FF2D95 0px 0px 20px"}}
+            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "#bbbbbb"}}
           >
             CoderHouse course, divided in data analytics and data science
+          </TimelineEvent>
+
+          <br />
+
+          <TimelineEvent
+            title="German"
+            titleStyle={{color: "#ffffff", fontWeight: 600, fontSize: "17px"}}
+            subtitle="2022-04-05"
+            subtitleStyle={{color: "#faebd7", fontWeight: 600}}
+            contentStyle={{backgroundColor: "background.default", color: "#bbbbbb", fontWeight: 600}}
+            icon={<HiTranslate size="1.5rem" color="rgb(2, 136, 209)" />}
+            bubbleStyle={{backgroundColor: "#1e1e1e", borderColor: "#bbbbbb"}}
+          >
+            Private class of german
           </TimelineEvent>
 
         </Timeline>
