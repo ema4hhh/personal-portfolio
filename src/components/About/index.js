326791e6d98ddbf739d106633cd570d9975737f3
ema4hhh <ema4hhh@gmail.com>
2022-08-06 00:04:59 -0500

    resposive component ans removed useless Grid

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,38 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Box, Grid, Toolbar, Typography, useTheme } from '@mui/material';
+import { Box, Toolbar, Typography, useTheme } from '@mui/material';
 
 const Intro = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
+  // eslint-disable-next-line no-unused-vars
   const theme = useTheme();
+
+  useEffect(() => {
+
+    if (window.innerWidth > 767) {
+      setIsMobile(false);
+    } else if (window.innerWidth < 767) {
+      setIsMobile(true);
+    }
+
+  }, [])
 
   return (
     <Box id="about" sx={{
       backgroundColor: "background.default",
       color: "text.primary",
-      }}>
+      width: "75%",
+      margin: "auto",
+    }}>
       <Toolbar id="go-to-content" />
 
-      <Box sx={{
-        width: "35rem",
-        margin: "auto",
+      <Typography 
+        variant='h1' 
+        sx={{
+        marginBottom: "3vh"
       }}>
-        <Typography variant='h1' sx={{
-          marginBottom: "3vh"
-        }}>Who is this guy?</Typography>
-        <Grid container>
-          <Grid item xs={12}>
-            <Typography variant='h4' sx={{
-              marginTop: "2vh",
-              textAlign: "center",
-              lineHeight: 1.5
-            }}>
-              Emanuel is a high school and CoderHouse student who is interested in full-stack development, data analytics, and data science. Emanuel's enthusiasm for technology and mathematics is contagious. He sees himself as a "eternal student" who is ready to both learn and help others.<br /> <br/>
-              Emanuel's thirst for knowledge, analytical ability, and resilience have lead him to a happier life, doing what he enjoys, coding.
-            </Typography> 
-          </Grid>
-        </Grid>
-      </Box>
+        Who is this guy?
+      </Typography>
+
+      <Typography variant='h4' sx={[
+        {marginTop: "2vh",
+        textAlign: "center",
+        lineHeight: 1.5},
+        isMobile ? {paddingInline: 0} : {paddingInline: "10em"}
+      ]}>
+        Emanuel is a high school and CoderHouse student who is interested about FullStack web development and data science. Emanuel's enthusiasm for technology and mathematics is contagious. He sees himself as a "eternal student" who is always willing to learn and experience new sensations.<br /> <br/>
+        Emanuel's thirst for knowledge, analytical ability, and resilience have lead him to a happier life, doing what he enjoys, coding.
+      </Typography> 
+
     </Box>
   )
 }
